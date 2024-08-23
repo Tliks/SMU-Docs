aa5a9d3e8ea1ba3650f7d1095d75a0cf043b6b06
tliks <aoft725@gmail.com>
2024-08-23 18:09:29 +0900

    Deploy website - based on d5e1daeae59fb4aae80b8d395934be64f9659fda

---
+++ en/assets/js/79d05521.278c0922.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[1018],{2840:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/SMU-Docs/en/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
